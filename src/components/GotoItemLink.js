2b4fa8ba70b790a1cf4ee6c224d947710d44b32b
Simon Eddy <simon@simoneddy.com.au>
2022-07-19 00:52:08 +1000

    more bogan css fixes

--- src/components/GotoItemLink.js
+++ src/components/GotoItemLink.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 function GotoItemLink({ children, to, className }) {
   return (
     <Link
-      className={`${className} m-2 text-3xl font-bold bg-blue-800 bg-opacity-20 hover:bg-red-900 hover:bg-opacity-10 active:bg-yellow-700`}
+      className={`${className} m-2 text-3xl font-bold bg-blue-800-op-20 hover:bg-red-900-op-10 active:bg-yellow-700-op-20`}
       to={to}
     >
       {children}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,38 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addComponents }) => {
+      addComponents({
+        '.bg-cyan-400-op-75': {
+          backgroundColor: 'rgba(34,211,238,0.75)'
+        },
+        '.bg-purple-200-op-30': {
+          backgroundColor: 'rgba(233,213,255,0.3)'
+        },
+        '.bg-purple-200-op-40': {
+          backgroundColor: 'rgba(233,213,255,0.4)'
+        },
+        '.bg-purple-200-op-60': {
+          backgroundColor: 'rgba(233,213,255,0.6)'
+        },
+        '.bg-purple-600-op-40': {
+          backgroundColor: 'rgba(147,51,234,0.4)'
+        },
+        '.bg-blue-800-op-20': {
+          backgroundColor: 'rgba(30,64,175,0.2)'
+        },
+        '.bg-red-900-op-10': {
+          backgroundColor: 'rgba(127,29,29,0.1)'
+        },
+        '.bg-yellow-700-op-20': {
+          backgroundColor: 'rgba(161,98,7,0.2)'
+        },
+      });
+    })
+  ],
 };
