9e5eeaf479542f415452c12d5cfb3cf16af331ff
Simon Eddy <simon@simoneddy.com.au>
2022-10-17 15:50:46 +1100

    using media api for display bg image

--- src/components/OuterContainer.js
+++ src/components/OuterContainer.js
@@ -1,13 +1,13 @@
-import { MEDIA_BASE_URI } from '../shared/consts';
+// import { MEDIA_BASE_URI } from '../shared/consts';
 
 function OuterContainer({ children }) {
   return (
     <div
       className="text-green-900 w-full h-full flex flex-col justify-start items-center"
-      style={{
-        background: `no-repeat url(${MEDIA_BASE_URI}Image2-bright.jpg)`,
-        backgroundSize: 'cover'
-      }}
+      // style={{
+      //   background: `no-repeat url(${MEDIA_BASE_URI}Image2-bright.jpg)`,
+      //   backgroundSize: 'cover'
+      // }}
     >
       {children}
     </div>
